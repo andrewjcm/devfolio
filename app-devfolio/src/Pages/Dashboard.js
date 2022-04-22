9e11e33b23fb5491cf98b98a3af6dac93163e19f
Andrew Metzgus <andrew@metzg.us>
2022-04-22 13:46:58 -0700

    Update to test functionality of auth system

--- app-devfolio/src/Pages/Dashboard.js
+++ app-devfolio/src/Pages/Dashboard.js
@@ -1,27 +1,47 @@
-import React from "react";
-import useRefreshToken from "../Hooks/useRefreshToken";
-
-// class Dashboard extends React.Component {
-
-//     const refresh = useRefreshToken();
-
-//     render() {
-//         return (
-//             <h1>Dashboard</h1>
-//             <button onClick={}></button>
-//         );
-//     }
-// }
-
-// export default Dashboard;
+import { useState, useEffect } from "react";
+import useAxiosPrivate from "../Hooks/useAxiosPrivate";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Dashboard = () => {
-    const refresh = useRefreshToken();
+    const [data, setData] = useState();
+    const axiosPrivate = useAxiosPrivate();
+    const navigate = useNavigate();
+    const location = useLocation();
 
-    return(
+
+    useEffect(() => {
+        let isMounted = true;
+        const controller = new AbortController();
+
+        const getUsers = async () => {
+            try {
+                const response = await axiosPrivate.get('users/');
+                isMounted && setData(response.data);
+            } catch (err) {
+                console.error(err);
+                navigate('/login', { state: { from: location }, replace: true });
+            }
+        }
+
+        getUsers();
+
+        return () => {
+            isMounted = false;
+            controller.abort();
+        }
+    }, [])
+
+    return (
         <div>
             <h1>Dashboard</h1>
-            <button onClick={() => refresh()}>Refresh</button>
+            {data?.length
+                ? (
+                    <ul>
+                        {Object.keys(data[0]).map(key => <li key={key}>{`${key}: ${data[0][key]}`}</li>)}
+                    </ul>
+                ) : <p>No users to display</p>
+            }
+
         </div>
     )
 }
