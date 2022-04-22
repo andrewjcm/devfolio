2cb02058fe60a18402f9774d05971558c1966aa4
Andrew Metzgus <andrew@metzg.us>
2022-04-22 13:46:20 -0700

    Update login routes and login actions

--- app-devfolio/src/Hooks/useRefreshToken.js
+++ app-devfolio/src/Hooks/useRefreshToken.js
@@ -1,21 +1,20 @@
-import axios from 'axios';
+import axios from '../Services/Api';
 import useAuth from './useAuth';
 
 const useRefreshToken = () => {
     const {auth, setAuth} = useAuth();
 
     const refresh = async () => {
-        console.log(auth.accessToken);
         const response = await axios.post(
-            'http://127.0.0.1:8000/api/token/refresh/', 
+            'token/refresh/', 
             {
                 "refresh": auth.refreshToken
             }
         );
-        const accessToken = response?.data?.access;
-        setAuth({accessToken: accessToken});
-        console.log(response.data.access);
-        console.log(auth.accessToken);
+        setAuth(auth => {
+            return {...auth, accessToken: response.data.access}
+        });
+
         return response.data.access;
     }
     return refresh;
